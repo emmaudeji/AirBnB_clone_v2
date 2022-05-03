13e48f2473217e2499d22a292e98536629224016
Emmanuel Udeji <88405403+emmaudeji@users.noreply.github.com>
2022-05-03 09:34:56 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
-# This script will set up my web servers for the deployment of web_static
+# Script to set up the web servers for the deployment of web_static
 
 echo -e "\e[1;32m START\e[0m"
 
 #--Updating the packages
 sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 echo -e "\e[1;32m Packages updated\e[0m"
 echo
 
-#--configure firewall
+#--configuring firewall
 sudo ufw allow 'Nginx HTTP'
 echo -e "\e[1;32m Allow incomming NGINX HTTP connections\e[0m"
 echo
 
-#--created the dir
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+#--creating the directrry
+sudo mkdir -p /data/web_static/releases/test/data/web_static/shared
 echo -e "\e[1;32m directories created"
 echo
 
-#--adds test string
+#--adding test string
 echo "<h1>Welcome to www.gogrene.tech</h1> <br> <h3>The tech guru</h3><br><h3>learning at alx/Holberton school</h3>" > /data/web_static/releases/test/index.html
 echo -e "\e[1;32m Test string added\e[0m"
 echo
